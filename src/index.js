a3d0b1f92770ab737c4792a41fe05aba69b6bc0a
Nathaly Gutierrez <nathy0239@gmail.com>
2021-09-09 14:15:05 -0300

    carousel

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+//import CarouselPizza from "./";
+//import CarouselPizza from "./components/CarouselPizzas";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App/>, document.getElementById("root"));
