5d0d340dd7bc27164a3ebf6a472b40f70d6c58f0
Roberto Villafa <metheoro4775@gmail.com>
2021-09-16 15:02:10 -0300

    actual

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Inicio from "./pages/Inicio";
 import Contacto from "./pages/Contacto";
 import Shop from "./pages/Shop";
+import Admin from "./pages/Admin"
+import Login from "./pages/Login";
 
 const App = () => {
 	return (
 		<Router>
 			<Switch>
+			    <Route exact path="/" component={Login} />
 				<Route exact path="/" component={Inicio} />
 				<Route exact path="/contacto" component={Contacto} />
 				<Route exact path="/shop" component={Shop} />
+				<Route exact path="/Admin" component={Admin} />
 			</Switch>
 		</Router>
 	);
